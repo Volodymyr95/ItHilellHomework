a67af8457eaa5cdd1c4f65f8b0518fc482f2857b
volodymyr_krokhmaliuk <krokhmalyuk777@gmail.com>
2022-09-19 19:32:51 +0300

    HW5: change hw oin homework

--- src/it/hiell/hw5/HW5Main.java
+++ src/it/hiell/hw5/HW5Main.java
@@ -2,7 +2,7 @@ package it.hiell.hw5;
 
 public class HW5Main {
     public static void main(String[] args) {
-        System.out.println("HW5");
+        System.out.println("Homework 5");
     }
 
 }
